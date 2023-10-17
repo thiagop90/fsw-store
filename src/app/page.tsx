5b7b3f5e03fa8dd156eb79e54ec026b9f5ced11a
Thiago <thgps01@outlook.com>
2023-10-17 20:43:40 -0300

    feat: add first banner to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,18 @@
 'use client'
 
+import Image from 'next/image'
+
 export default function Home() {
-  return <h1></h1>
+  return (
+    <div className="p-5">
+      <Image
+        src="/banner-home-01.png"
+        width={0}
+        height={0}
+        className="h-auto w-full"
+        sizes="100vw"
+        alt="Até 55% de desconto esse mês."
+      />
+    </div>
+  )
 }
