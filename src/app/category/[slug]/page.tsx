7ed134e35b35b8b1fe7365634dd1d1f86e174ba1
Thiago <thgps01@outlook.com>
2023-10-18 12:18:06 -0300

    style: button change to link

--- src/app/category/[slug]/page.tsx
+++ src/app/category/[slug]/page.tsx
@@ -3,6 +3,8 @@ import { Badge } from '@/components/ui/badge'
 import { categoryIcon } from '@/constants/category-icon'
 import { computeProductTotalPrice } from '@/helpers/products'
 import { prismaClient } from '@/lib/prisma'
+import { ChevronLeft } from 'lucide-react'
+import Link from 'next/link'
 
 type CategoryItemProps = {
   params: {
@@ -26,13 +28,21 @@ export default async function CategoryProducts({ params }: CategoryItemProps) {
 
   return (
     <div className="p-5">
-      <Badge
-        className="gap-1 border-primary px-3 py-1.5 text-base uppercase"
-        variant="outline"
-      >
-        {categoryIcon[params.slug as keyof typeof categoryIcon]}
-        {category.name}
-      </Badge>
+      <div className="flex items-center gap-2">
+        <Link
+          href="/catalog"
+          className="rounded-full border border-primary p-2"
+        >
+          <ChevronLeft className="h-4 w-4" />
+        </Link>
+        <Badge
+          className="gap-1 border-primary px-3 py-1.5 text-base uppercase"
+          variant="outline"
+        >
+          {categoryIcon[params.slug as keyof typeof categoryIcon]}
+          {category.name}
+        </Badge>
+      </div>
 
       <div className="mt-8 grid grid-cols-2 gap-6">
         {category.products.map((product) => (
