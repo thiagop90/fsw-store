73137ae8604c2a0e3fa67c625a7a1e2ac5464217
Thiago <thgps01@outlook.com>
2023-10-17 22:26:38 -0300

    feat: add mouses section to home page

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -21,6 +21,14 @@ export default async function Home() {
     },
   })
 
+  const mouses = await prismaClient.product.findMany({
+    where: {
+      category: {
+        slug: 'mouses',
+      },
+    },
+  })
+
   return (
     <div className="flex flex-col gap-8 py-6">
       <PromoBanner
@@ -51,6 +59,11 @@ export default async function Home() {
         src="/banner-home-03.png"
         alt="Até 55% de desconto em mouses!"
       />
+
+      <div>
+        <SectionTitle>Mouses</SectionTitle>
+        <ProductList products={mouses} />
+      </div>
     </div>
   )
 }
